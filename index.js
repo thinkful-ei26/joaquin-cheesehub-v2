de8df13bd57550eb9caa2001261014b40b4a7f58
Joaquin Fox <joaquin.fox@gmail.com>
2018-12-22 11:14:20 -0700

    server setup

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
-
+const cheeseRouter = require('./routes/cheese')
+app.use(express.json());
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,28 +22,41 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+app.use('/api/cheeses', cheeseRouter);
 
-app.get('/api/cheeses', (req, res, next) => {
-  return res.json([
-    'Bath Blue',
-    'Barkham Blue',
-    'Buxton Blue',
-    'Cheshire Blue',
-    'Devon Blue',
-    'Dorset Blue Vinney',
-    'Dovedale',
-    'Exmoor Blue',
-    'Harbourne Blue',
-    'Lanark Blue',
-    'Lymeswold',
-    'Oxford Blue',
-    'Shropshire Blue',
-    'Stichelton',
-    'Stilton',
-    'Blue Wensleydale',
-    'Yorkshire Blue'
-  ]);
+app.use('/', (req, res, next)=>{
+  const err = new Error("Route not found");
+    err.status = 404;
+    err.reason = "Invalid path";
+    return next(err);
 });
+app.use('*', (err, req, res, next)=>{
+  err.message = err.message || "Unknown Error";
+  err.status = err.status || 500;
+  res.status(err.status).json({message : err.message});
+});
+
+// app.get('/api/cheeses', (req, res, next) => {
+//   return res.json([
+//     'Bath Blue',
+//     'Barkham Blue',
+//     'Buxton Blue',
+//     'Cheshire Blue',
+//     'Devon Blue',
+//     'Dorset Blue Vinney',
+//     'Dovedale',
+//     'Exmoor Blue',
+//     'Harbourne Blue',
+//     'Lanark Blue',
+//     'Lymeswold',
+//     'Oxford Blue',
+//     'Shropshire Blue',
+//     'Stichelton',
+//     'Stilton',
+//     'Blue Wensleydale',
+//     'Yorkshire Blue'
+//   ]);
+// });
 
 // app.get('/api/cheeses', (req, res, next) => {
 //   res.json({ msg: 'Hanga Banga' });
--- routes/cheese.js
+++ routes/cheese.js
@@ -1,4 +1,61 @@
 'use strict';
 const express = require('express');
 const Router = express.Router();
-const store = require('../store')
+const store = require('../store');
+const {
+  isString: validateString,
+  howMuchCheese,
+  oldCheese,
+  whoMovedMyCheese
+} = require('../validation');
+const { Cheese } = require('../models/cheese');
+
+//get a list of all cheeses, no tokens needed
+Router.get('/', (req, res, next) => {
+  Cheese.find().then(data => res.json(data));
+});
+
+//add a new cheese to the server list, also no token for now
+Router.post('/', (req, res, next) => {
+  const { name } = req.body;
+  //validate valididitty
+  if (whoMovedMyCheese(name)) {
+    //cheese not found :(
+    const err = new Error('Bad Request');
+    err.message = 'You forgot the cheese!';
+    err.status = 400;
+    return next(err);
+  }
+  if (!validateString(name)) {
+    //here is bad cheese
+    const err = new Error('Bad Request');
+    err.message = 'All New Cheese MUST be stringy..';
+    err.status = 400;
+    return next(err);
+  }
+  const lotsOfCheese = howMuchCheese(name);
+  if (lotsOfCheese) {
+    //here is wrong amount of cheese
+    const err = new Error('Bad Request');
+    err.message = lotsOfCheese;
+    err.status = 400;
+    return next(err);
+  }
+  const usedCheese = oldCheese(name);
+  if (usedCheese) {
+    //we dont want this cheese, thanks
+    const err = new Error('Bad Request');
+    err.message = 'We dont want this cheese again, thanks';
+    err.status = 400;
+    return next(err);
+  }
+  //passed cheese-checks, here is good cheese
+  // store.addCheese(newCheese);
+  Cheese.create({ name: name })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => {});
+});
+
+module.exports = Router;
